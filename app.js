6b6dbf59af99ba98c97ba25d4a0a0f89c82190bc
Zach Bazov <zachbazov@gmail.com>
2021-03-02 22:39:41 +0200

    routing

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const compression = require('compression');
+const fs = require('fs');
 
 const app = express();
 
 app.use(compression());
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
+        results: tours.length,
+        data: tours,
     });
 });
 
