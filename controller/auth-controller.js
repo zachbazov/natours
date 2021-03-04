9776f27e95597caa47ae91d2f952f3c73b81fd8c
Zach Bazov <zachbazov@gmail.com>
2021-03-05 00:30:05 +0200

    implemented user creation handler

---
+++ controller/auth-controller.js
@@ -0,0 +1,11 @@
+const User = require('../model/user-model');
+const catchAsync = require('../utils/catch-async');
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data: { user: newUser }
+    });
+});
--- route/user-routes.js
+++ route/user-routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const userController = require('../controller/user-controller');
+const authController = require('../controller/auth-controller');
 
 const router = express.Router();
+
+router
+    .post('/signup', authController.signup);
 
 router
     .route('/')
