fdef36892c2f9907b02870267dc3362bf5a92ad7
Zach Bazov <zachbazov@gmail.com>
2021-03-04 15:04:23 +0200

    implemented global error handling class/controller

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
-
+const AppError = require('./utils/app-error');
+const errorController = require('./controller/error-controller');
 const tourRouter = require('./route/tour-routes');
 const userRouter = require('./route/user-routes');
 
-// Error Handling
-// In case a route hasn't found on the server.
-app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'failure',
-        message: `Can't find ${req.originalUrl} on the server.`
-    });
-});
+const app = express();
 
 // Non-development-dependency - development logging middleware.
 if (process.env.NODE_ENV === 'development')
@@ -33,5 +26,22 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Error Handling
+// In case a route hasn't found on the server.
+app.all('*', (req, res, next) => {
+    // const err = new Error(`Can't find ${req.originalUrl} on the server.`);
+    // err.status = 'failure';
+    // err.statusCode = 404;
+    //next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on the server.`, 404));
+});
+
+// Global Error Handling Middleware
+// In case we pass an argument to the next() function.
+// Express will recognize it as an error.
+// It skips all the middlewares on the middleware stack.
+// Sends the argument that passed in, to the global error handling middleware.
+app.use(errorController);
 
 module.exports = app;
