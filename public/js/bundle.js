b259642369540c1d302ce34d282ad435012a5ce3
Zach Bazov <zachbazov@gmail.com>
2021-03-08 23:13:23 +0200

    implemented error based on node environment

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8830,7 +8830,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56677" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49715" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
