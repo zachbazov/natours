aa1ee97e7a24cf4322d66233d78443b7fc8c7650
Zach Bazov <zachbazov@gmail.com>
2021-03-08 03:42:54 +0200

    implemented mapbox feature for displaying tour location for the user

--- controller/view-controller.js
+++ controller/view-controller.js
@@ -1,9 +1,11 @@
 const catchAsync = require('../utils/catch-async');
 const Tour = require('../model/tour-model');
 
+const CSP_PERMISSIONS = `default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;`;
+
 exports.getOverview = catchAsync(async (req, res, next) => {
     const tours = await Tour.find();
-    res.status(200).render('overview', {
+    res.status(200).set('Content-Security-Policy', CSP_PERMISSIONS).render('overview', {
         title: 'All tours',
         tours
     });
@@ -15,7 +17,7 @@ exports.getTour = catchAsync(async (req, res) => {
         path: 'reviews',
         select: 'review rating user'
     });
-    res.status(200).render('tour', {
+    res.status(200).set('Content-Security-Policy', CSP_PERMISSIONS).render('tour', {
         title: tour.name,
         tour
     });
