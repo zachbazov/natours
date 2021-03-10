e55546d940365955c6c611f883f882bb654505b6
Zach Bazov <zachbazov@gmail.com>
2021-03-11 01:42:17 +0200

    improved stripe implementation

--- controller/booking-controller.js
+++ controller/booking-controller.js
@@ -9,31 +9,31 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //Gets the currently booked tour.
     const tour = await Tour.findById(req.params.tourId);
-    console.log(req.params.tourId);
+    console.log(tour);
     if (!tour) return next(new AppError('No tour found for checkout session.', 400));
     // Creates a checkout session.
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        //success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
-        success_url: `${req.protocol}://${req.get('host')}/my-bookings`,
-        cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
-        customer_email: req.user.email,
-        client_reference_id: req.params.tourId,
-        line_items: [{
-            name: `${tour.name} Tour`,
-            description: `${tour.summary}`,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
-            amount: tour.price * 100,
-            currency: 'usd',
-            quantity: 1
-        }]
-    });
-    console.log(session);
-    // Creates a session as response.
-    res.status(200).json({
-        status: 'success',
-        session
-    });
+    // const session = await stripe.checkout.sessions.create({
+    //     payment_method_types: ['card'],
+    //     //success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+    //     success_url: `${req.protocol}://${req.get('host')}/my-bookings`,
+    //     cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
+    //     customer_email: req.user.email,
+    //     client_reference_id: req.params.tourId,
+    //     line_items: [{
+    //         name: `${tour.name} Tour`,
+    //         description: `${tour.summary}`,
+    //         images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+    //         amount: tour.price * 100,
+    //         currency: 'usd',
+    //         quantity: 1
+    //     }]
+    // });
+    // console.log(session);
+    // // Creates a session as response.
+    // res.status(200).json({
+    //     status: 'success',
+    //     session
+    // });
 });
 
 // exports.createBookingCheckout = catchAsync(async (req, res, next) => {
