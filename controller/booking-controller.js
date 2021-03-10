c212031a57ab81985cca70c74252375ee650744e
Zach Bazov <zachbazov@gmail.com>
2021-03-10 23:16:42 +0200

    improved stripe implementation

--- controller/booking-controller.js
+++ controller/booking-controller.js
@@ -54,6 +54,7 @@ const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
     const price = session.line_items[0].amount / 100;
+    console.log(tour, user, price);
     await Booking.create({ tour, user, price });
 };
 
